2453bb2167c3f4e13067d6d8063d6cf09ab2460b
cse <cseshahrukh@gmail.com>
2023-08-06 12:17:19 +0600

    render database

--- Backend/routes/auth/app.py
+++ Backend/routes/auth/app.py
@@ -4,6 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import func
 import datetime
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 
 import os
@@ -11,6 +18,8 @@ import psycopg2
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+
 api = Api(app)
 
 # Load environment variables
@@ -24,6 +33,9 @@ conn = psycopg2.connect(db_url, sslmode='prefer')
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# For authentication
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+bcrypt = Bcrypt(app)
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
 
@@ -32,10 +44,99 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+@app.route('/')
+def home():
+    print('Inside home function')
+    return render_template('home.html')
+
+@ app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = form.password.data
+        #hashed_password = bcrypt.generate_password_hash(form.password.data)
+        #hashed_password = hashed_password.decode("utf-8", "ignore")
+        
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    print('Inside login function')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            #if bcrypt.check_password_hash(user.password, form.password.data):
+            if user.password == form.password.data:
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard/', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
 
 
 if __name__ == '__main__':
--- Backend/run.py
+++
@@ -1,4 +0,0 @@
-from backendapp import app
-
-if __name__ == '__main__':
-    app.run(debug=True)

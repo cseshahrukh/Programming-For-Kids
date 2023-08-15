22b7bc1e01d3e9a394639e0f69b31b36fa8f56c6
cseshahrukh <cseshahrukh@gmail.com>
2023-08-15 22:15:23 +0600

    mcq problem done

--- Backend/routes/auth/app.py
+++ Backend/routes/auth/app.py
@@ -165,8 +165,87 @@ class WeeklyModules(db.Model):
         return f"WeeklyModules(weeklymodule_id={self.weeklymodule_id}, module_name={self.module_name})"
 
 
+class MCQ(db.Model):
+    __tablename__ = 'MCQ'
+    
+    mcq_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    question = db.Column(db.String(1000), nullable=False)
+    correct_option = db.Column(db.String(1), nullable=False)  # Use a string to store the correct option label
+    explanation = db.Column(db.String(1000))
 
+    
+    # Course association
+    course_id = db.Column(db.Integer, db.ForeignKey('Course.course_id'), nullable=False)
+    week_no = db.Column(db.Integer, nullable=False)
+    lesson_no = db.Column(db.Integer, nullable=False)
+    course = db.relationship('Course', back_populates='mcqs')
+    
+    options = db.relationship('MCQOption', back_populates='mcq', cascade='all, delete-orphan')
+    
+    def __repr__(self):
+        return f"MCQ(mcq_id={self.mcq_id}, question={self.question})"
+    
+    def to_dict(self):
+        return {
+            'mcq_id': self.mcq_id,
+            'question': self.question,
+            'correct_option': self.correct_option,
+            'explanation': self.explanation,
+            'course_id': self.course_id,
+            'options': [option.to_dict() for option in self.options]
+        }
 
+class MCQOption(db.Model):
+    __tablename__ = 'MCQOption'
+    
+    option_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    option_label = db.Column(db.String(1), nullable=False)  # Use a string to store the option label (A, B, C, etc.)
+    option_text = db.Column(db.String(500), nullable=False)
+    mcq_id = db.Column(db.Integer, db.ForeignKey('MCQ.mcq_id'), nullable=False)
+    
+    mcq = db.relationship('MCQ', back_populates='options')
+    
+    def to_dict(self):
+        return {
+            'option_id': self.option_id,
+            'option_label': self.option_label,
+            'option_text': self.option_text,
+            'mcq_id': self.mcq_id
+        }
+
+class ProgrammingProblem(db.Model):
+    __tablename__ = 'ProgrammingProblem'
+    
+    # Define the columns for the ProgrammingProblem table
+    problem_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    problem = db.Column(db.String(1000), nullable=False)
+    sample_input = db.Column(db.String(500), nullable=False)
+    sample_output = db.Column(db.String(500), nullable=False)
+    testing_input = db.Column(db.String(1000), nullable=False)
+    testing_output = db.Column(db.String(1000), nullable=False)
+    points = db.Column(db.Integer, nullable=False)
+    lesson_no = db.Column(db.Integer, nullable=False)
+    week_no = db.Column(db.Integer, nullable=False)
+    
+    # Course association
+    course_id = db.Column(db.Integer, db.ForeignKey('Course.course_id'), nullable=False)
+    course = db.relationship('Course', back_populates='programming_problems')
+    
+    def __repr__(self):
+        return f"ProgrammingProblem(problem_id={self.problem_id}, problem={self.problem})"
+    
+    def to_dict(self):
+        return {
+            'problem_id': self.problem_id,
+            'problem': self.problem,
+            'sample_input': self.sample_input,
+            'sample_output': self.sample_output,
+            'testing_input': self.testing_input,
+            'testing_output': self.testing_output,
+            'points': self.points,
+            'course_id': self.course_id
+        }
+    
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[

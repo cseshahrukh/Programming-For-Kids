679f51354a7b09d3605723b63a692675526f128e
TAHMID-AT <tahmid.ahanaf98@gmail.com>
2023-08-21 00:42:41 +0600

    Programming Problems

--- Frontend/src/Pages/Program.js
+++ Frontend/src/Pages/Program.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import './ProgrammingProblem.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import CodeEditor from './CodeEditor';
 
-const Programming = () => {
+let currentIndex = 1;
 
+const Programming = () => {
   const { course_id, week_no, lesson_no } = useParams();
   const [problemsList, setProblemsList] = useState([]);
-  const [currentMCQIndex, setCurrentMCQIndex] = useState(0);
-  const currentProgram = problemsList[currentMCQIndex];
   const [error, setError] = useState(false);
 
   useEffect(() => {
     // fetch(`/courses/problems/${course_id}/${week_no}/${lesson_no}`)
-    fetch(`/courses/problems/101/1/1`)
+    fetch(`/courses/problems/${course_id}/${week_no}/${lesson_no}`)
       .then(response => {
         if (!response.ok) {
           setError(true);
@@ -37,7 +36,7 @@ const Programming = () => {
     return <p>Loading...</p>;
   }
 
-  console.log(problemsList)
+  const currentProgram = problemsList[currentIndex]; // You should adjust this according to your data structure
 
   return (
     <div className="App">
@@ -49,32 +48,24 @@ const Programming = () => {
           <div className="problem-description">
             <h2>Problem Description</h2>
             <p>
-              Given an array of integers, find the maximum possible sum you can get by adding non-adjacent elements. Write a function that takes an array of integers as input and returns the maximum sum.
+              {currentProgram.problemDescription}
             </p>
           </div>
           <div className="test-case-container">
             <h3>Test Cases</h3>
             <div className="test-cases">
-              <div className="test-case">
-                <div className="test-input">
-                  <p><strong>Input:</strong></p>
-                  <pre>[2, 4, 6, 2, 5]</pre>
+              {currentProgram.testCases.map((testCase, index) => (
+                <div className="test-case" key={index}>
+                  <div className="test-input">
+                    <p><strong>Input:</strong></p>
+                    <pre>{JSON.stringify(testCase.input)}</pre>
+                  </div>
+                  <div className="test-output">
+                    <p><strong>Output:</strong></p>
+                    <pre>{JSON.stringify(testCase.output)}</pre>
+                  </div>
                 </div>
-                <div className="test-output">
-                  <p><strong>Output:</strong></p>
-                  <pre>13</pre>
-                </div>
-              </div>
-              <div className="test-case">
-                <div className="test-input">
-                  <p><strong>Input:</strong></p>
-                  <pre>[5, 1, 1, 5]</pre>
-                </div>
-                <div className="test-output">
-                  <p><strong>Output:</strong></p>
-                  <pre>10</pre>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>

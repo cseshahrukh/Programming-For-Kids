c35908e627eec818ceb1227b28fe94e3091900ea
Sohidul1234 <133635854+Sohidul1234@users.noreply.github.com>
2023-09-10 21:44:30 +0600

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -953,8 +953,8 @@ def get_completed_courses():
 
 
 # Running app
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
 
 
 #DELETE FROM reading_materials
